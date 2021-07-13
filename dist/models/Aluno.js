37c53ab4350ca217cf70eeef49d19c587a6f0171
wilnir <wilnir@hotmail.com>
2021-07-13 12:14:45 -0300

    adding

---
+++ dist/models/Aluno.js
@@ -0,0 +1,74 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _sequelize = require('sequelize'); var _sequelize2 = _interopRequireDefault(_sequelize);
+
+ class Aluno extends _sequelize.Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'nome precisa ter entre 2 e 255',
+          },
+        },
+      },
+      sobrenome: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'sobrenome precisa ter entre 2 e 255',
+          },
+        },
+      },
+      email: {
+        type: _sequelize2.default.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'email ja existe',
+        },
+        validate: {
+          isEmail: {
+            msg: 'email INVALIDO',
+          },
+        },
+      },
+      idade: {
+        type: _sequelize2.default.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser inteiro',
+          },
+        },
+      },
+      peso: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa ser x,x ou x',
+          },
+        },
+      },
+      altura: {
+        type: _sequelize2.default.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa ser x,x ou x',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.Foto, { foreignKey: 'aluno_id' });
+  }
+} exports.default = Aluno;
